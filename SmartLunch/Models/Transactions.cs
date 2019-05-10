0db11ef7d076f5dfaf439515af98582a0f6a4de5
Dan <dan.semerak@gmail.com>
2019-05-11 02:01:31 +0300

    first

---
+++ SmartLunch/Models/Transactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLunch.Models
+{
+    public class Transactions
+    {
+        public int id_tr { get; set; }
+        public int t_id_client { get; set; }
+        public DateTime t_time { get; set; }
+        public decimal balance { get; set; }
+        public string assignment { get; set; }
+
+        //public Clients Client { get; set; }
+    }
+}
