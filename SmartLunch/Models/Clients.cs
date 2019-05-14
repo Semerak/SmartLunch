744736fc19d20e5ec471b65dcd622df47fc9a03d
Dan <dan.semerak@gmail.com>
2019-05-14 20:42:22 +0300

    model start

---
+++ SmartLunch/Models/Clients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLunch.Models
+{
+    public class Clients
+    {
+        public int ID { get; set; }
+       // public int c_id_client { get; set; }
+        public string last_name { get; set; }
+        public string first_name { get; set; }
+        public string father_name { get; set; }
+        public int FormsID { get; set; }
+        public string email { get; set; }
+        public string pass { get; set; }
+        public string code { get; set; }
+
+        public Forms Forms { get; set; }
+    }
+}
+ 
---
+++ SmartLunch/Models/Forms.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartLunch.Models
+{
+    public class Forms
+    {
+
+        public int ID { get; set; }
+        public string name_from { get; set; }
+
+        public ICollection<Clients> Clients { get; set; }
+    }
+}
